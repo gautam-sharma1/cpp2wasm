5e9bee2d22f888254a5981c80eb7ecd3c69958c8
Gautam <gautamsharma2813@gmail.com>
2024-01-17 06:23:39 -0500

    changed5

--- app/components/GoogleAnalytics.jsx
+++ app/components/GoogleAnalytics.jsx
@@ -4,10 +4,9 @@ export default function GoogleAnalytics() {
     return (
         <>
             <Script
-                prefetch
                 src="https://www.googletagmanager.com/gtag/js?id=G-Q7HMTR5N95"
             ></Script>
-            <Script prefetch>
+            <Script>
                 {`  window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
