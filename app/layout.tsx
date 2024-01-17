d8477500f6561f5e08ae23481bfd4c60e37c1dfc
Gautam <gautamsharma2813@gmail.com>
2024-01-17 05:33:22 -0500

    added gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import Script from "next/script";
 import { Inter } from "next/font/google";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
+import Head from "next/head";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -18,17 +20,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="dracula">
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-Q7HMTR5N95"
-      ></Script>
-      <Script>
-        {`  window.dataLayer = window.dataLayer || [];
+      <Head>
+        <Script
+          strategy="beforeInteractive"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Q7HMTR5N95"
+        ></Script>
+        <Script strategy="beforeInteractive">
+          {`  window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-Q7HMTR5N95');`}
-      </Script>
+        </Script>
+      </Head>
       <body className={inter.className}>
         <div className="flex flex-col items-center px-12 pt-12">
           <Header />
